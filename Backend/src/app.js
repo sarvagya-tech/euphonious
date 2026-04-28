d290f301a91695183ea7169f17a73fd949e000b3
sarvagya-tech <sarvagyam93@gmail.com>
2026-04-28 13:34:07 +0530

    Fix chatRoom router import and update error handling in app.js; add chatRoom routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,39 +1,32 @@
 import express from 'express'
 import cookieparser from 'cookie-parser'
 import cors from 'cors'
+import userRouter from './routes/user.routes.js'
+import songRouter from './routes/songs.routes.js'
+import chatRoomrouter from './routes/cahtRoom.routes.js'
 
-const app = express();
+const app = express()
 
-app.use(cookieparser());
+app.use(cookieparser())
 app.use(cors({
-      origin: 'http://localhost:5173',
-      credentials : true
+    origin: 'http://localhost:5173',
+    credentials: true
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
-import userRouter from './routes/user.routes.js';
-import songRouter from './routes/songs.routes.js';
-
-app.use("/api/v1/songs",songRouter);
-
-app.use("/api/v1/users",userRouter);
+app.use("/api/v1/songs", songRouter)
+app.use("/api/v1/users", userRouter)
+app.use("api/v1/chatRoom",chatRoomrouter)
 
 app.use((err, req, res, next) => {
-      if (res.headersSent) {
-            return next(err);
-      }
+    const statusCode = err.statuscode || 500
+    const message = err.message || "Internal Server Error"
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        errors: err.error || [],
+    })
+})
 
-      const statusCode = err.statuscode || err.statusCode || err.status || 500;
-      const message = err.message || "Internal Server Error";
-
-      return res.status(statusCode).json({
-            success: false,
-            message,
-            errors: err.error || [],
-      });
-});
-
-
-export {app};
+export { app }
---
+++ Backend/src/routes/cahtRoom.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { createchatRoom, deleteRoom, getcurrentRoom, joiningRoom, leaveRoom } from "../controller/chatRoom.controller.js";
+import { verifyJwt } from "../middleware/Auth.middleware.js";
+
+const chatRoomrouter = Router();
+
+chatRoomrouter.post('/create',verifyJwt,createchatRoom);
+chatRoomrouter.post('/:roomId/join',verifyJwt,joiningRoom);
+chatRoomrouter.post('/:roomId/leave',verifyJwt,leaveRoom);
+chatRoomrouter.delete('/:roomId',verifyJwt,deleteRoom);
+chatRoomrouter.get('/:roomId',verifyJwt,getcurrentRoom);
+
+export default chatRoomrouter;

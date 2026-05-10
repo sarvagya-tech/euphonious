8c5742ed8ce455ffca9c4db838bbb0ad3d978cf9
sarvagya-tech <sarvagyam93@gmail.com>
2026-05-10 20:18:10 +0530

    feat: initialize frontend project and implement socket.io client utility functions

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -2,25 +2,67 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = "http://localhost:7000"
 
-export const socket = io(SOCKET_URL, {
+const socket = io(SOCKET_URL, {
     autoConnect: false,
     withCredentials: true
 })
 
-export const
+export const connectSocket = () => {
+    if (!socket.connected) {
+        socket.connect();
+    }
+};
+export const disconnectSocket = () => {
+    if (socket.connected) {
+        socket.disconnect();
+    }
+};
 
+export const joinRoom = (roomId, userId) => {
+    socket.emit("joinRoom", { roomId, userId });
+};
 
+export const leaveRoom = () => {
+    socket.emit("leaveRoom");
+};
 
+export const sendMessage = (roomId, userId, message) => {
+    socket.emit("sendMessage", { roomId, userId, message });
 
+};
 
+export const syncPlay = (roomId, songUrl, position) => {
+    socket.emit("playSong", { roomId, songUrl, position });
 
+};
 
+export const syncPause = (roomId, position) => {
+    socket.emit("pauseSong", { roomId, position });
 
+};
 
-
-
-
-
-
+export const syncSeek = (roomId, position) => {
+    socket.emit("seekSong", { roomId, position });
+};
+export const syncSkip = (roomId, songUrl) => {
+    socket.emit("skipSong", { roomId, songUrl });
+};
 
 export default socket;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
